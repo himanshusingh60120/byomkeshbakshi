06c4bb860912b12a88010f79e10257a8d433092d
Himanshu Singh <100782501+himanshusingh60120@users.noreply.github.com>
2026-09-01 18:21:54 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: { bodySizeLimit: '2mb' },
+  },
+};
+
+export default nextConfig;
